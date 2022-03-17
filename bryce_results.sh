f16b9faa49eddd50fc9bedbef03439edebb3bf85
Bryce Johnson <joh14192@umn.edu>
2022-03-17 11:02:03 -0500

    my sh script

--- bryce_results.sh
+++ bryce_results.sh
@@ -4,7 +4,5 @@ echo "Bash version ${BASH_VERSION}..."
 for i in {2..10}
   do
      a=$((i*24))
-     python src/evaluate.py BLAT_ECOLX_Ranganathan2015-2500 ev --n_seeds=1 --n_threads=1 --n_train=$a
+     python src/evaluate.py BLAT_ECOLX_Ranganathan2015-2500 onehot --n_seeds=1 --n_threads=1 --n_train=$a
   done
-
-
