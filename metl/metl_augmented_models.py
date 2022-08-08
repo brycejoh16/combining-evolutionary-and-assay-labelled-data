05ee77c4905ff9feaee3319a8dabe1e4ddb44849
brycejoh16 <joh14192@umn.edu>
2022-08-08 09:36:27 -0500

    augmented model script updates

--- metl/metl_augmented_models.py
+++ metl/metl_augmented_models.py
@@ -1,13 +1,19 @@
 '''
+Author: Bryce Johnson
+Gitter Lab ~ August 5th , 2022
 code augmented from https://github.com/chloechsu/combining-evolutionary-and-assay-labelled-data
-to implement
+to implement augmented models from precomputed density features and manually define test and train splits
 '''
 
 import numpy as np
+import pandas as pd
 from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import Ridge
 from scipy.stats import spearmanr
 from sklearn.metrics import make_scorer
+import os
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 def get_decoupled_mutants(mutants ,offset=0 ,split_character=':'):
     ans =[]
@@ -18,12 +24,15 @@ def get_decoupled_mutants(mutants ,offset=0 ,split_character=':'):
     return ans
 
 def mutant2sequence(mutant,WT,offset, split_character=':'):
+    # todo: check this works for multiple sequences
     decouple_mutants=get_decoupled_mutants(mutant,offset,split_character)
     new_seq=WT
     for dc_mutant in decouple_mutants:
         original,idx,new=dc_mutant[0],dc_mutant[1],dc_mutant[2]
         assert new_seq[idx]==original, f'original mutant ({original}) at index {idx} not the same as that in wildtype'
-        new_seq[idx]=new
+        temp = list(new_seq)
+        temp[idx] = new
+        new_seq = "".join(temp)
     return new_seq
 
 def seqs_to_onehot(seqs):
@@ -111,12 +120,30 @@ aa_to_int = {
 REG_COEF_LIST = [1e-1, 1e0, 1e1, 1e2, 1e3]
 
 class AugmentedModel():
-    def __init__(self ,WT ,reg_coef=1,offset=0,split_character=':'):
+    def __init__(self ,WT,reg_coef=1,offset=0,split_character=':'):
         self.reg_coef =reg_coef
         self.WT =WT
         self.offset=offset
         self.split_character=split_character
-    def train(self, mutants, DMS_scores, density_feature=None, reg_coef_density_feature=None, reg_coef_onehot=1):
+    def train(self, mutants, DMS_score, density_feature=None, reg_coef_density_feature=1e-8, reg_coef_onehot=1):
+        '''
+        training function for augmented model
+        :param mutants: pd.Series of all mutants e.g. pd.Series(['W24P',...])
+        :param DMS_score: pd.Series of associated DMS_scores
+        :param density_feature: optional -- pd.Series of associated density features
+        :param reg_coef_density_feature: regularization coefficient for the density specific feature
+        :param reg_coef_onehot: regularization coefficient for the onehot values
+        :return:
+        '''
+        if density_feature is not None:
+            assert sum(np.isnan(density_feature))==0, 'accidentally passed in some density features which where nan'
+            assert type(density_feature) == pd.Series, 'density feature must be a pandas series'
+            assert len(mutants)==len(density_feature),'density feature must be same length as mutants'
+        assert sum(np.isnan(DMS_score))==0,'accidentally passed in some DMS_scores which where nan'
+        assert len(mutants) == len(DMS_score), 'mutants must be the same lenght as DMS_score'
+        assert type(DMS_score) == pd.Series,'dms score must be a pandas series'
+        assert type(mutants)==pd.Series,'mutants must be a pandas series'
+
         # set feature specific regularization coefficients
         self.reg_coef_onehot = reg_coef_onehot
         self.reg_coef_density_feature = reg_coef_density_feature
@@ -125,12 +152,12 @@ class AugmentedModel():
         features=self.features(mutants,density_feature)
 
         # cross validation
-        if self.reg_coef is None or self.reg_coef == 'CV':
-            self.find_optimal_reg_coef(features,DMS_scores)
+        if self.reg_coef == 'CV':
+            self.find_optimal_reg_coef(features,DMS_score)
 
         # set the model
         self.model = Ridge(alpha=self.reg_coef)
-        self.model.fit(features, DMS_scores.to_numpy())
+        self.model.fit(features, DMS_score.to_numpy())
 
 
     def predict(self ,mutants ,density_feature=None):
@@ -138,14 +165,15 @@ class AugmentedModel():
         return self.model.predict(features)
 
     def features(self,mutants,density_feature):
+
         sequences = mutants.apply(
             lambda x: mutant2sequence(x, WT=self.WT, offset=self.offset, split_character=self.split_character))
         features = seqs_to_onehot(sequences) * np.sqrt(1.0 / self.reg_coef_onehot)
+        # todo: makes sure onehot encoding works for multiple sequences
         if density_feature is not None:
             assert self.reg_coef_density_feature is not None, 'Why is reg_coef of the density feature None'
-            density_feature_numpy = density_feature.to_numpy() * np.sqrt(1.0 / self.reg_coef_density_feature)
-            # todo: check this again
-            features = np.concatenate([features, density_feature_numpy], axis=1)
+            density_feature_numpy = density_feature.to_numpy().reshape(-1,1)* np.sqrt(1.0 / self.reg_coef_density_feature)
+            features = np.concatenate([features,density_feature_numpy],axis=1)
         return features
     def find_optimal_reg_coef(self,features,DMS_scores):
         best_rc, best_score = None, -np.inf
@@ -159,19 +187,68 @@ class AugmentedModel():
                 best_rc = rc
                 best_score = score
         self.reg_coef = best_rc
+    def __repr__(self):
+        return f"ridge reg_coef {self.reg_coef},onehot reg_ceof {self.reg_coef_onehot},density reg_coef {self.reg_coef_density_feature}"
 
 
+def learning_curve(ag,train,test,nb_train,nb_simulations,density_features):
+  df_spearman=pd.DataFrame()
+  df_spearman['nb_train']=nb_train
+  df_spearman=df_spearman.set_index('nb_train')
 
-        # if density_features is not None:
-        #     density_columns=density_features.columns()
-        #     if reg_coef_density_features is None:
-        #         reg_coef_density_features = [1]*len(density_columns)
-        #     assert len(density_columns) ==len(reg_coef_density_features), 'number of density columns must equal the number of regression coefficients'
-        #     for col in density_columns:
-        #         density_features[]
+  for density_col_name in tqdm(density_features):
+      S=[]
+      for i in nb_train:
+          s=[]
+          for _ in range(nb_simulations):
+              train_i=train.sample(n=i)
+              if density_col_name is 'onehot':
+                  df_train = None
+                  df_test = None
+              else:
+                  df_train = train_i[density_col_name]
+                  df_test = test[density_col_name]
+
+              # if you want to mess with the reg_coef of density features individually here is
+              # where you would do it.
+              ag.train(mutants=train_i['mutant'],DMS_score=train_i['DMS_score'],density_feature=df_train)
+              predicted=ag.predict(mutants=test['mutant'],density_feature=df_test)
+              s.append(spearman(predicted,test['DMS_score']))
+          S.append(np.array(s).mean())
+
+      df_spearman[density_col_name]=S
+  return df_spearman
+def main():
+    dataset='SPG1_STRSG_Olson_2014'
+    beta_filename=os.path.join('databases','protein_gym_benchmark',f"{dataset}.csv")
+    df=pd.read_csv(beta_filename)
+    WT='MQYKLILNGKTLKGETTTEAVDAATAEKVFKQYANDNGVDGEWTYDDATKTFTVTE'
+    ag=AugmentedModel(WT,offset=227,split_character=":",reg_coef=1)
+    # print(f"can keep {sum(~np.isnan(df['EVmutation']))} sequences out of {len(df)}")
+    #if following Tranception evaluation must remove all points EVmutation can't predict on for all datasets
+    df=df[~np.isnan(df['EVE_single'])]
+    frac=0.1
+    test = df.sample(frac=frac, random_state=1)  # so im hard coding my test state,
+    # but not my split on which points to sample from.
+    test = test.copy()
+    train = df.drop(test.index)
 
 
+    ##### ==========problem set-up complete ========================
 
-        # multiply by the factor for the augmented factors (maybe make a dictionary for this)
+    # make a learning curve for one density feature
+    df_spearman=pd.DataFrame()
+    nb_train=np.arange(1,6,1)*48
+    nb_simulations=1
 
-        #
+    ax=df_spearman.plot.line(marker='x')
+    ax.set_xlabel('nb training points')
+    ax.set_xticks(nb_train)
+    ax.set_title(f'{dataset}\n'
+                 f'{ag}')
+    # ax.set_yticks([0,.25,.5,.75,1])
+    plt.legend()
+    plt.savefig(os.path.join('results','learning_curves',f'{dataset}_frac_{frac}.png'))
+
+if __name__=='__main__':
+    main()
